f72b9a3b5b66c06f25ad5818fc31a26a349bc2e0
MonserrathAranda <nahomimartinez300@gmail.com>
2023-10-15 20:05:53 -0600

    Agregar nuevo componente "imagen"

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import MenuHamburguesa from './BurgerMenu';
 import './MainPage.css'
+import corte2 from './imagenes/corte2.jpg'
 
 const MainPage = ({ onLogout }) => {
   return (
@@ -8,6 +9,7 @@ const MainPage = ({ onLogout }) => {
         <MenuHamburguesa />
         <br/>
       <div className='contenedor2'><h1 >Bienvenido a mi Pagina Principal</h1></div>
+      <img src={corte2}></img>
       <div className='contenedor'><button  onClick={onLogout}>Cerrar sesión</button></div>
 
     </div>
