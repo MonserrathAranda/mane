9c99e976b82409cbd1dffb76fe8582e55f3184da
MonserrathAranda <nahomimartinez300@gmail.com>
2023-10-15 20:10:05 -0600

    Agregar segundo elemento "imagen 2"

--- src/MainPage.js
+++ src/MainPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import MenuHamburguesa from './BurgerMenu';
 import './MainPage.css'
 import corte2 from './imagenes/corte2.jpg'
+import corte4 from './imagenes/corte4.jpg'
 
 const MainPage = ({ onLogout }) => {
   return (
@@ -9,8 +10,9 @@ const MainPage = ({ onLogout }) => {
         <MenuHamburguesa />
         <br/>
       <div className='contenedor2'><h1 >Bienvenido a mi Pagina Principal</h1></div>
-      <img src={corte2}></img>
+      <img src={corte2}></img> <img src={corte4}></img>
       <div className='contenedor'><button  onClick={onLogout}>Cerrar sesión</button></div>
+      
 
     </div>
   );
